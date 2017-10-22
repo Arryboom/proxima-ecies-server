efbc45a5fe859ad9e8492ce38ac6515c43add9ac
sebelik <Sebelik@Ondrejs-MacBook.local>
2017-10-22 17:41:38 +0200

    Use O2-Czech-Republic/pexeso-ecies-go as base ECIES implementation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
@@ -8,13 +9,13 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/O2-Czech-Republic/pexeso-ecies-go"
 	"github.com/gorilla/mux"
 )
 
 func DecryptMessage(w http.ResponseWriter, req *http.Request) {
 
 	message, err := base64.StdEncoding.DecodeString(req.URL.Query().Get("message"))
-	sharedParam1, err := base64.StdEncoding.DecodeString(req.URL.Query().Get("iv"))
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -22,7 +23,7 @@ func DecryptMessage(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	ownPrivateKey, err := LoadPrivateKey("./server.key.pem")
+	ownPrivateKey, err := LoadPrivateKey("./server.key.ec.pem")
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -30,8 +31,16 @@ func DecryptMessage(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	ownPrivateKeyEC := ImportECDSA(ownPrivateKey)
-	decrypted, err := ownPrivateKeyEC.Decrypt(rand.Reader, message, sharedParam1, nil)
+	var sharedParam1 []byte
+
+	if req.URL.Query().Get("iv") != "" {
+		sharedParam1, _ = base64.StdEncoding.DecodeString(req.URL.Query().Get("iv"))
+	} else {
+		sharedParam1 = nil
+	}
+
+	ownPrivateKeyEC := ecies.ImportECDSA(ownPrivateKey)
+	decrypted, err := ownPrivateKeyEC.Decrypt(bytes.NewReader(sharedParam1), message, nil, nil)
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -58,32 +67,23 @@ func EncryptMessage(w http.ResponseWriter, req *http.Request) {
 
 	messageBytes := []byte(message)
 	peerPublicKey, err := LoadPublicKey("./server.pub.pem")
-	peerPublicKeyEC := ImportECDSAPublic(peerPublicKey)
+	peerPublicKeyEC := ecies.ImportECDSAPublic(peerPublicKey)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	var sharedParam1 []byte
-
-	if req.URL.Query().Get("iv") != "" {
-		sharedParam1 = make([]byte, 16)
-		rand.Read(sharedParam1)
-	} else {
-		sharedParam1 = nil
-	}
-
-	encrypted, err := Encrypt(rand.Reader, peerPublicKeyEC, messageBytes, sharedParam1, nil)
+	encrypted, err := ecies.Encrypt(rand.Reader, peerPublicKeyEC, messageBytes, nil, nil)
 	baseSF := base64.StdEncoding.EncodeToString(encrypted)
 
 	if w != nil {
 		w.Header().Set("Content-type", "text/plain")
 		_, _ = w.Write([]byte("Plain Base64:\n"))
 		_, _ = w.Write([]byte("Ciphertext: " + baseSF + "\n"))
-		_, _ = w.Write([]byte("Shared Param (IV): " + base64.StdEncoding.EncodeToString(sharedParam1) + "\n"))
+		//_, _ = w.Write([]byte("Shared Param (IV): " + base64.StdEncoding.EncodeToString(sharedParam1) + "\n"))
 		_, _ = w.Write([]byte("\n\nURL-encoded:\n"))
 		_, _ = w.Write([]byte("Ciphertext: " + url.QueryEscape(baseSF) + "\n"))
-		_, _ = w.Write([]byte("Shared Param (IV): " + url.QueryEscape(base64.StdEncoding.EncodeToString(sharedParam1)) + "\n"))
+		//_, _ = w.Write([]byte("Shared Param (IV): " + url.QueryEscape(base64.StdEncoding.EncodeToString(sharedParam1)) + "\n"))
 	}
 
 }
